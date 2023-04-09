3ed1f81c6adcb4eb74d93245fd993df43ed4c5ff
uchqun007 <uchqunjonturdiev@gmail.com>
2023-04-09 17:17:00 +0500

    React-multi-carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import '@fontsource/roboto';
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import 'react-multi-carousel/lib/styles.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
