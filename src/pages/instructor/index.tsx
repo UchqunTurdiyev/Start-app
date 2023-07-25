b9f99526c70f1c00d1cf44a18bd579fb1d29fd2a
uchqun007 <uchqunjonturdiev@gmail.com>
2023-07-25 18:21:41 +0500

    Instructor page sidebar

---
+++ src/pages/instructor/index.tsx
@@ -0,0 +1,8 @@
+import { withInstructorLayout } from '@/layouts/instructor';
+import React from 'react';
+
+function InstructorPage() {
+	return <div>InstructorPage</div>;
+}
+
+export default withInstructorLayout(InstructorPage);
