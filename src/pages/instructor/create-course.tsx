34d9f69b395cc20ce9f869ee983533a1791351ee
uchqun007 <uchqunjonturdiev@gmail.com>
2023-07-26 17:25:48 +0500

    Instructor Create-course-component page

--- src/pages/instructor/create-course.tsx
+++ src/pages/instructor/create-course.tsx
@@ -1,9 +1,10 @@
 import { withInstructorLayout } from '@/layouts/instructor';
+import { InstructorCreateCourseComponent } from '@/page-component';
 import { NextPage } from 'next';
 import React from 'react';
 
 const CreateCourse: NextPage = () => {
-	return <div>CreateCourse</div>;
+	return <InstructorCreateCourseComponent />;
 };
 
 export default withInstructorLayout(CreateCourse);
